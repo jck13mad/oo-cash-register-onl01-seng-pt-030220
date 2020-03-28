b6176ae55b4685ef4f0384dd890745c1b01285b2
Jack Madden <jck13mad@gmail.com>
2020-03-28 17:34:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,6 +19,8 @@ class CashRegister
     if discount != 0 
       @total -= (@total * (@discount / 100.to_f))
       puts "After the discount, the total comes out to be #{@total.to_i}."
+    else
+      
     end
   end
   
