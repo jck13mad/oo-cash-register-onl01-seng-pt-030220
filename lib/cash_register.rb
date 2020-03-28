55a76b00497ef40a678fee3c20d00b969d32b468
Jack Madden <jck13mad@gmail.com>
2020-03-28 17:28:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@ class CashRegister
   
   attr_accessor :total, :discount, :item, :last_transaction
   
-  def intitialize(discount = 0)
+  def intitialize(discount=0)
     @total = 0 
     @item = []
     @discount = discount
